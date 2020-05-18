e20a38ccedf01c7c9d0ae06b4f02f4c6fae7aadd
Jack Bremner <j.z.bremner@dundee.ac.uk>
2020-05-18 16:58:11 +0100

    minor variable name change

--- fo4_component_scraper.py
+++ fo4_component_scraper.py
@@ -12,12 +12,12 @@ def get_components():
     # Get the first table on the page (which contains the components and their IDs)
     components_table = soup.find('table', class_='va-table')
     # Get all the rows from the table, as a list
-    components_rows = components_table.find_all('tr')
+    component_rows = components_table.find_all('tr')
 
     # Loop through each row and extract the component name and base ID,
     # excluding first 2 rows as they are headers
     components = {}
-    for component in components_rows[2:]:
+    for component in component_rows[2:]:
         name = component.a.text.lower()
         item_base_id = component.find('span', class_='va-formid').text.upper()
         components[name] = item_base_id
